fd3aef6411e37afb9c54761ec5c9e5cc84c4ce8d
HamzaMaaliki <hamzamaaliki07@gmail.com>
2026-08-14 21:04:49 +0300

    feat(web): add dashboard

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import Dashboard from './MainDashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/messages.tsx
@@ -0,0 +1,14 @@
+function Messages() {
+  return <h1>hello gg world</h1>;
+}
+
+export default function App() {
+  return (
+    <div className="p-8">
+      <Messages />
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
